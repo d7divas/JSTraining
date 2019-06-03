daa3a43f58dd9094cc766147725bd72d3671bc2c
d7divas <31116603+d7divas@users.noreply.github.com>
2019-06-03 10:27:56 -0600

    Add files via upload

---
+++ Array-1.js
@@ -0,0 +1,36 @@
+const UnitList = [
+  {make: "Harley Davidson", type: "motorcyle", year: "2019"},
+  {make: "Jayco", type: "Motorhome", year: "2018"},
+  {make: "John Deere", type: "Tractor", year: "2039"},
+  {make: "John Deere", type: "Combine", year: "2017"},
+  {make: "Vanderhall", type: "Trike", year: "2018"},
+  {make: "Coachmen", type: "Motorhome", year: "2050"},
+  {make: "Brent", type: "Combine", year: "2017"},
+];
+
+/*
+ * Instructions: Given a array of units, use a loop to go through each unit.
+ *   For each unit you will:
+ *     1. Create a function that takes list as argument
+ *     2. extract the "make", "type", "year" and assign each to an arry
+ *     3. if the unit "year" is greater than or equal to "2039" assign the 
+ *        string "Future tech" to a variable named "specialValue". If not
+ *        then assign the string "Contemporary" to "specialValue".
+ *     4. create a string comprised of joining the "make", "type", "year" and
+ *        "specialValue".
+ *     5. The function should return an object comprised of the array of the "makes",
+ *        "types", "years", and concatenated string from step "4"
+*/
+
+
+
+function fxnArray(UnitList){
+	
+	var make = [];
+	var type = [];
+	var year = [];
+	
+	UnitList.forEach(function() {
+		make.push
+	});
+}
